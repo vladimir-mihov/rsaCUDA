c25b3aaa6696f399e05ba60e66869cd2851c7911
vladimir-mihov <kols@abv.bg>
2018-05-23 13:32:37 +0300

    cosmetic fix

--- main.cu
+++ main.cu
@@ -2,6 +2,7 @@
 #include <chrono>
 #include <math.h>
 #include <vector>
+#include <cstdint>
 #include "mandelbrotData.hpp"
 #include "parser/parser.hpp"
 #include "lodepng/lodepng.h"
@@ -9,7 +10,6 @@
 #define NOW chrono::high_resolution_clock::now()
 #define DRAW
 
-using uchar = unsigned char;
 using namespace std;
 
 #define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
@@ -21,7 +21,7 @@ inline void gpuAssert(cudaError_t code, const char *file, int line)
    }
 }
 
-__global__ void mandelbrot( mandelbrotData *data, uchar *result ) {
+__global__ void mandelbrot( mandelbrotData *data, uint8_t *result ) {
 	int	index = blockDim.x*blockIdx.x + threadIdx.x,
 		col = index % data->width,
 		row = index / data->width;
@@ -33,7 +33,7 @@ __global__ void mandelbrot( mandelbrotData *data, uchar *result ) {
 			c_im = data->startY - row*data->stepY,
 			x = 0, y = 0;
 
-	uchar iterations = 0;
+	uint8_t iterations = 0;
 
 	while( x*x+y*y < 4 && iterations < 255 )
 	{
@@ -49,12 +49,12 @@ __global__ void mandelbrot( mandelbrotData *data, uchar *result ) {
 }
 
 #ifdef DRAW
-void writePNG( uchar *result, programOptions& opts );
+void writePNG( uint8_t *result, programOptions& opts );
 #endif
 
 int main( int argc, char ** argv ) {
 	// initial variables
-	uchar *result, *d_result;
+	uint8_t *result, *d_result;
 	mandelbrotData data, *d_data;
 	programOptions opts;
 
@@ -71,10 +71,10 @@ int main( int argc, char ** argv ) {
 	}
 	cout << (opts.verbose ? "Done parsing command line arguments.\n" : "");
 
-	result = new uchar[data.pixels];
+	result = new uint8_t[data.pixels];
 
 	cout << (opts.verbose ? "Allocating memory on the GPU.\n" : "");
-	gpuErrchk( cudaMalloc((void **)&d_result, data.pixels*sizeof(uchar)) );
+	gpuErrchk( cudaMalloc((void **)&d_result, data.pixels) );
 	gpuErrchk( cudaMalloc((void **)&d_data, sizeof(mandelbrotData)) );
 
 	gpuErrchk( cudaMemcpy( d_data, &data, sizeof(mandelbrotData), cudaMemcpyHostToDevice ) );
@@ -86,7 +86,7 @@ int main( int argc, char ** argv ) {
 	auto t2 = NOW;
 	cout << (opts.verbose ? "Done. It took " : "") << chrono::duration<double,milli>(t2-t1).count() << " ms.\n";
 
-	gpuErrchk( cudaMemcpy( result, d_result, data.pixels*sizeof(uchar), cudaMemcpyDeviceToHost ) );
+	gpuErrchk( cudaMemcpy( result, d_result, data.pixels, cudaMemcpyDeviceToHost ) );
 
 #ifdef DRAW
 	cout << (opts.verbose ? "Generating png image.\n" : "");
@@ -105,21 +105,21 @@ int main( int argc, char ** argv ) {
 }
 
 #ifdef DRAW
-void writePNG( uchar *result, programOptions& opts )
+void writePNG( uint8_t *result, programOptions& opts )
 {
 	int w = opts.width, h = opts.height;
-	vector<uchar> rawPixelData(w*h*4);
+	vector<unsigned char> rawPixelData(w*h*4);
 
-	int	setColor =  (opts.setColor & 0xff0000)>>16 | (opts.setColor & 0xff00) | (opts.setColor & 0xff)<<16 | 0xff<<24,
-		nonSetColor1 = (opts.nonSetColor1 & 0xff0000)>>16 | (opts.nonSetColor1 & 0xff00) | (opts.nonSetColor1 & 0xff)<<16 | 0xff<<24,
-		nonSetColor2 = (opts.nonSetColor2 & 0xff0000)>>16 | (opts.nonSetColor2 & 0xff00) | (opts.nonSetColor2 & 0xff)<<16 | 0xff<<24;
+	uint32_t	setColor =  (opts.setColor & 0xff0000)>>16 | (opts.setColor & 0xff00) | (opts.setColor & 0xff)<<16 | 0xff<<24,
+				nonSetColor1 = (opts.nonSetColor1 & 0xff0000)>>16 | (opts.nonSetColor1 & 0xff00) | (opts.nonSetColor1 & 0xff)<<16 | 0xff<<24,
+				nonSetColor2 = (opts.nonSetColor2 & 0xff0000)>>16 | (opts.nonSetColor2 & 0xff00) | (opts.nonSetColor2 & 0xff)<<16 | 0xff<<24;
 
 	for (int y = 0; y < h; ++y)
 		for (int x = 0; x < w; ++x)
 		{
 			int index = 4*w*y + 4*x;
-			uchar resultElement = result[y*w+x];
-			*reinterpret_cast<int*>(&rawPixelData[index]) = resultElement == 255 ? setColor : ( resultElement % 2 ? nonSetColor1 : nonSetColor2 );
+			uint8_t resultElement = result[y*w+x];
+			*reinterpret_cast<uint32_t*>(&rawPixelData[index]) = resultElement == 255 ? setColor : ( resultElement % 2 ? nonSetColor1 : nonSetColor2 );
 		}
 	unsigned int error = lodepng::encode( opts.outputFilename.c_str(), rawPixelData, w, h );
 	if( error ) cerr << "encoder error " << error << ": " << lodepng_error_text(error) << endl;
